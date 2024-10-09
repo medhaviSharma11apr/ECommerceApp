4b414545e8eb7ce430d89a18a5d850bcce1472a0
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2024-10-09 18:57:00 +0530

    first commit

---
+++ src/components/FormField.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-form>
+     <InputTextField
+      :placeHolderText="EnterName"
+     ></InputTextField>
+
+      <InputTextField
+      :placeHolderText="EnterEmail"
+     ></InputTextField>
+     {{ EnterName }}
+    </v-form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import InputTextField from './InputTextField.vue';
+
+const EnterName = ref('enter name');
+const EnterEmail = ref('enter email');
+
+</script>
---
+++ src/components/InputTextField.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <v-text-field v-model="inputText" :label="placeHolderText">
+    </v-text-field>
+   
+</template>
+
+<script >
+
+
+
+export default {
+    props: {
+    placeHolderText :String,
+   }
+};
+
+</script>
+
+
+
+<style></style>
